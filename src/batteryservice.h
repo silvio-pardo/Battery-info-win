a1fde775789738517724fb2ba2346c964f4935d4
SilvioPardo <silviopardo@outlook.it>
2022-02-09 01:38:02 +0100

    add system info func

--- src/batteryservice.h
+++ src/batteryservice.h
@@ -7,16 +7,23 @@
 #include <iostream>
 #include <array>
 #include <vector>
+
+#define _WIN32_DCOM
+#include <ole2.h>
+#include <oleauto.h>
+#include <comdef.h>
+#include <wbemidl.h>
+#pragma comment(lib, "wbemuuid.lib")
+
 #include <napi.h>
 
 class BatteryService : public Napi::ObjectWrap<BatteryService>
 {
 public:
     BatteryService(const Napi::CallbackInfo& info);
-    
     struct BatteryInfo {
         //error while retrieve infos of single battery
-        bool errors;
+        bool errors = false;
         std::string device_name;
         ULONG designed_capacity;
         ULONG full_charged_capacity;
@@ -27,11 +34,19 @@ public:
         LONG rate;
     };
 
-    //std::vector<BatteryInfo>
+    struct SystemInfo {
+		bool errors = false;
+		std::string model;
+		std::string name;
+		std::string manufacturer;
+	};
+
     Napi::Value getBatteryList(const Napi::CallbackInfo&);
+    Napi::Value getSystemInfo(const Napi::CallbackInfo&);
 
     static Napi::Function GetClass(Napi::Env);
 
 protected:
     BatteryInfo getBatteryInfo(const HDEVINFO& hd_ev, SP_DEVICE_INTERFACE_DATA& sp_device);
+    SystemInfo getSystemData();
 };
--- test/test_bindings.js
+++ test/test_bindings.js
@@ -6,10 +6,16 @@ assert(BatteryService, "The expected module is undefined");
 function testBasic()
 {
     const instance = new BatteryService();
+    //batterylist
     assert(instance.getBatteryList, "The expected method is not defined");
     const returnObject = instance.getBatteryList();
     console.log(returnObject);
     assert.ok(returnObject != null, "Unexpected value returned");
+    //systeminfo
+    assert(instance.getSystemInfo, "The expected method is not defined");
+    const returnObject_1 = instance.getSystemInfo();
+    console.log(returnObject_1);
+    assert.ok(returnObject_1 != null, "Unexpected value returned");
 }
 
 assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
